7c76906042bcf6763afb7085cea1dd4748c41425
Safae88 <safaebouchouicha@gmail.com>
2026-03-13 22:18:25 +0000

    fix routes error

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running...");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
- const authRoutes = require("./routes/AuthRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 app.use("/api/auth", authRoutes);
